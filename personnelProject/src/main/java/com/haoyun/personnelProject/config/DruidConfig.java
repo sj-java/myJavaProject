d7da6a88f0662fa7619774664e92a3b9da60e852
ylzt <547578508@qq.com>
2019-09-06 13:52:23 +0800

    support idea and add git ignore

--- personnelProject/src/main/java/com/haoyun/personnelProject/config/DruidConfig.java
+++ personnelProject/src/main/java/com/haoyun/personnelProject/config/DruidConfig.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import com.haoyun.personnelProject.util.XulUtils;
 import org.aopalliance.intercept.Interceptor;
 import org.apache.ibatis.io.VFS;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -27,48 +28,50 @@ import com.github.pagehelper.PageHelper;
 
 @Configuration
 @EnableTransactionManagement
-public class DruidConfig implements EnvironmentAware{
-	
-	private static final Logger logger=LoggerFactory.getLogger(DruidConfig.class);
-	
-	private Environment environment;
-	
-	@Override
-	public void setEnvironment(Environment environment) {
-		this.environment = environment;
-	}
-	@Bean
-	public DataSource  druidDataSource() {
-		DruidDataSource datasource=new DruidDataSource();
-		try {
-			datasource.setUrl(environment.getProperty("spring.datasource.url"));
-	        datasource.setUsername(environment.getProperty("spring.datasource.username"));
-	        datasource.setPassword(environment.getProperty("spring.datasource.password"));
-	        datasource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
-	        datasource.setInitialSize(Integer.parseInt(environment.getProperty("spring.datasource.initialSize")));
-	        datasource.setMinIdle(Integer.parseInt(environment.getProperty("spring.datasource.minIdle")));
-	        datasource.setMaxActive(Integer.parseInt(environment.getProperty("spring.datasource.maxActive")));
-	        datasource.setMaxWait(Integer.parseInt(environment.getProperty("spring.datasource.maxWait")));
-	        datasource.setTimeBetweenEvictionRunsMillis(Integer.parseInt(environment.getProperty("spring.datasource.timeBetweenEvictionRunsMillis")));
-	        datasource.setMinEvictableIdleTimeMillis(Integer.parseInt(environment.getProperty("spring.datasource.minEvictableIdleTimeMillis")));
-	        datasource.setValidationQuery(environment.getProperty("spring.datasource.validationQuery"));
-	        datasource.setTestWhileIdle(Boolean.getBoolean(environment.getProperty("spring.datasource.testWhileIdle")));
-	        datasource.setTestOnBorrow(Boolean.getBoolean(environment.getProperty("spring.datasource.testOnBorrow")));
-	        datasource.setTestOnReturn(Boolean.getBoolean(environment.getProperty("spring.datasource.testOnReturn")));
-        
+public class DruidConfig implements EnvironmentAware {
+
+    private static final Logger logger = LoggerFactory.getLogger(DruidConfig.class);
+
+    private Environment environment;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource druidDataSource() {
+        DruidDataSource datasource = new DruidDataSource();
+        try {
+            datasource.setUrl(environment.getProperty("spring.datasource.url"));
+            datasource.setUsername(environment.getProperty("spring.datasource.username"));
+            datasource.setPassword(environment.getProperty("spring.datasource.password"));
+            datasource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+            datasource.setInitialSize(XulUtils.tryParseInt(environment.getProperty("spring.datasource.initialSize")));
+            datasource.setMinIdle(XulUtils.tryParseInt(environment.getProperty("spring.datasource.minIdle")));
+            datasource.setMaxActive(XulUtils.tryParseInt(environment.getProperty("spring.datasource.maxActive"),20));
+            datasource.setMaxWait(XulUtils.tryParseInt(environment.getProperty("spring.datasource.maxWait")));
+            datasource.setTimeBetweenEvictionRunsMillis(XulUtils.tryParseInt(environment.getProperty("spring.datasource.timeBetweenEvictionRunsMillis")));
+            datasource.setMinEvictableIdleTimeMillis(XulUtils.tryParseInt(environment.getProperty("spring.datasource.minEvictableIdleTimeMillis")));
+            datasource.setValidationQuery(environment.getProperty("spring.datasource.validationQuery"));
+            datasource.setTestWhileIdle(Boolean.getBoolean(environment.getProperty("spring.datasource.testWhileIdle")));
+            datasource.setTestOnBorrow(Boolean.getBoolean(environment.getProperty("spring.datasource.testOnBorrow")));
+            datasource.setTestOnReturn(Boolean.getBoolean(environment.getProperty("spring.datasource.testOnReturn")));
+
             datasource.setFilters(environment.getProperty("spring.datasource.filters"));
             logger.info("DuridConfig-----数据源完成");
         } catch (SQLException e) {
             logger.error("druid configuration initialization filter", e);
         }
-        
-		return datasource;
-	}
-	@Bean
-	public SqlSessionFactory sqlSessionFactory() {
-		SqlSessionFactoryBean  sqlSessionFactoryBean=new SqlSessionFactoryBean();
-		sqlSessionFactoryBean.setDataSource(druidDataSource());
-		//mybayis分页插件
+
+        return datasource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(druidDataSource());
+        //mybayis分页插件
 		/*PageHelper pageHelper=new PageHelper();
 		Properties props=new Properties();
 		props.setProperty("dialect", "mysql");
@@ -82,16 +85,16 @@ public class DruidConfig implements EnvironmentAware{
         VFS.addImplClass(SpringBootVFS.class);
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         try {
-			sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/com/haoyun/personnelProject/mapper/*.xml"));
-			 logger.info("dao层扫描包为:classpath:/com/haoyun/personnelProject/mapper/*.xml");
-			 return sqlSessionFactoryBean.getObject();
-		} catch (Exception e) {
-			logger.info("classpath:/com/haoyun/personnelProject/mapper/*.xml路径未找到");
-			e.printStackTrace();
-		}
+            sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/com/haoyun/personnelProject/mapper/*.xml"));
+            logger.info("dao层扫描包为:classpath:/com/haoyun/personnelProject/mapper/*.xml");
+            return sqlSessionFactoryBean.getObject();
+        } catch (Exception e) {
+            logger.info("classpath:/com/haoyun/personnelProject/mapper/*.xml路径未找到");
+            e.printStackTrace();
+        }
         return null;
-	}
-	//开启事务管理器
+    }
+    //开启事务管理器
 	/*@Bean
 	public PlatformTransactionManager transactionManager() throws SQLException {
 	        return new DataSourceTransactionManager(druidDataSource());
